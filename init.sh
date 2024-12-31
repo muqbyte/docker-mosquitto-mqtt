e0291966449c597e5ae4bc76fa891fa77fdc197b
muqtadir <tadir.muqtadir@gmail.com>
2024-12-31 11:15:39 +0800

    add the mqtt_url

--- init.sh
+++ init.sh
@@ -15,14 +15,16 @@ EOL
 #create a directory -p /etc/mosquitto/conf.d
 sudo mkdir -p /etc/mosquitto/conf.d
 
+read -p "Please enter your MQTT URL: " mqtt_url
+
 cat << EOL > /etc/mosquitto/conf.d/default.conf
 listener 1883
 allow_anonymous true
 
 listener 8883
-certfile /etc/letsencrypt/live/mqtt.muqtadir.site/cert.pem
-cafile /etc/letsencrypt/live/mqtt.muqtadir.site/chain.pem
-keyfile /etc/letsencrypt/live/mqtt.muqtadir.site/privkey.pem
+certfile /etc/letsencrypt/live/$mqtt_url/cert.pem
+cafile /etc/letsencrypt/live/$mqtt_url/chain.pem
+keyfile /etc/letsencrypt/live/$mqtt_url/privkey.pem
 EOL
 
 sudo docker-compose up -d
